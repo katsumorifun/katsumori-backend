73d527473828cb75480bc60392a030e591301930
Daniil <den4ic2001@gmail.com>
2022-05-29 17:20:19 +0300

    В модель пользователя добавлены новые поля: пол, описание(обо мне)

---
+++ src/database/migrations/2022_05_29_141018_add_description_and_gender_field_in_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('description')->nullable(true)->default(null);
+            $table->enum('gender', ['male', 'female', 'other'])->nullable(true)->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('description');
+            $table->dropColumn('gender');
+        });
+    }
+};
