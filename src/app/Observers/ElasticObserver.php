3110dc8756b95848721a9802483ecdd5935c8cf0
Daniil <den4ic2001@gmail.com>
2022-10-12 20:40:31 +0300

    Обновлен наблюдатель Elasticsearch

--- src/app/Observers/ElasticObserver.php
+++ src/app/Observers/ElasticObserver.php
@@ -49,23 +49,4 @@ class ElasticObserver
             'id' => $model->getKey(),
         ]);
     }
-
-    /**
-     * Handle the User "updated" event.
-     *
-     * @param $model
-     * @return void
-     * @throws \Elastic\Elasticsearch\Exception\ClientResponseException
-     * @throws \Elastic\Elasticsearch\Exception\MissingParameterException
-     * @throws \Elastic\Elasticsearch\Exception\ServerResponseException
-     */
-    public function updated($model): void
-    {
-        $this->elastic->update([
-            'id' => $model->getKey(),
-            'body' => $model->toSearchArray(),
-            'type' => $model->getSearchType(),
-            'index' => $model->getSearchIndex(),
-        ]);
-    }
 }
